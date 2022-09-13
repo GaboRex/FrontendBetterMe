345d433de7bca1c8514fa2fd6553d71a947180b3
GaboRex <gaboneme11@gmail.com>
2022-09-12 22:23:44 -0400

    Fix inicio

--- src/Paginas/Inicio/inicio.js
+++ src/Paginas/Inicio/inicio.js
@@ -14,6 +14,10 @@ const Init = () => {
         </button>
         </Link>
     </div>
+    <br /><br /><br /><br /><br /><br />
+    <br /><br /><br /><br /><br /><br />
+    <br /><br /><br /><br /><br /><br />
+    <br /><br /><br /><br /><br /><br />
   </div>
     )
 }
